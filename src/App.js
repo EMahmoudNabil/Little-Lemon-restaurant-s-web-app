33060e54eba0a498a5c14c9a440e352b88af8ea4
Mahmoud Nabil <e.mahmoudnabil@gmail.com>
2023-01-09 21:40:04 +0200

    final project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello in resturant app</div>;
+  return (
+    <>
+      <nav>
+        <img src="../image/Logo.svg" alt="Logo" />
+        <ul>
+          <li>
+            <a href="/home">Homepage</a>
+          </li>
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+        </ul>
+      </nav>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
